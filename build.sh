27edd33ce8c9ff3ca2231d3b1408adb229d7bf38
jiraffarij <felipe.martins4@hotmail.com>
2023-03-21 18:27:05 -0300

    fixing render deploy

--- build.sh
+++ build.sh
@@ -6,12 +6,13 @@ CPATH=/opt/render/project/python/Python-3.10.10/include/python3.10/
 
 pip install -r requirements.txt
 
-
-python manage.py makemigrations allauth
-python manage.py makemigrations posts
-python manage.py makemigrations accounts
-python manage.py makemigrations
+#
+#python manage.py makemigrations allauth
+#python manage.py makemigrations posts
+#python manage.py makemigrations accounts
+#python manage.py makemigrations
 
 python manage.py collectstatic --no-input
+#python manage.py migrate sites
 python manage.py migrate
 python manage.py createsuperuser --no-input
